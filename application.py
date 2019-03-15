17bf73d93af604d61e84b6ee3adce52e9c42cc44
TEOHAROV Filip <Filip.TEOHAROV@akka.eu>
2019-03-15 15:14:47 +0100

    Dai mu da vyrvu

---
+++ application.py
@@ -0,0 +1,9 @@
+from flask import Flask
+application = Flask(__name__)
+
+@application.route('/')
+def hello_world():
+   return "Hello Jenkins Automatic arive me "
+
+if __name__ == '__main__':
+   application.run(debug = True)
