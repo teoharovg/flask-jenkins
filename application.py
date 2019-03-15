435ee08eecfa5c4d310b11d882bb3f101a7987bb
TEOHAROV Filip <Filip.TEOHAROV@akka.eu>
2019-03-15 15:10:00 +0100

    Dano da raboti

--- application.py
+++ application.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route('/')
 def hello_world():
-   return "Hello World"
+   return "Jenkins after push"
 
 if __name__ == '__main__':
    application.run(debug = True)
