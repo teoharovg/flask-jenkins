f85aa2aacbde1aca3e1454222c73e316cdb48079
teoharovg <35068951+teoharovg@users.noreply.github.com>
2019-03-15 15:51:22 +0100

    Update application.py

--- application.py
+++ application.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route('/')
 def hello_world():
-   return "Hello Jenkins Automatic arive me111 "
+   return "Hello Jenkins daaaaiiii "
 
 if __name__ == '__main__':
    application.run(debug = True)
