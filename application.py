5a865d06fdd836ebe9a7da16b45e7ece5b45caca
teoharovg <35068951+teoharovg@users.noreply.github.com>
2019-08-03 17:53:43 +0300

    Update application.py

--- application.py
+++ application.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route('/')
 def hello_world():
-   return "Hello Jenkins vsichko raboti "
+   return "Hello Jenkins vsichko не работи "
 
 if __name__ == '__main__':
    application.run(host = '0.0.0.0', port=3030)
